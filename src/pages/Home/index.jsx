83acecd67029e835a37e833c4bbd1185a4546cd1
yuki <abbq100@163.com>
2021-11-28 16:46:14 -0800

    add container and init modal template

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,50 @@
+import './style.css';
+
+import { useState } from 'react';
+import { Button, Tooltip, Layout, Modal } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { CreateModal } from '../../components/CreateModal';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const Home = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <Layout className="page-container">
+      <Header>Use it before waste it!</Header>
+      <Content>
+        <Tooltip title="Add a New Item" className="add-new-item">
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<PlusOutlined />}
+            onClick={showModal}
+          />
+        </Tooltip>
+        <Modal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <CreateModal />
+        </Modal>
+      </Content>
+    </Layout>
+  );
+};
+
+export default Home;
